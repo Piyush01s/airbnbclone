637058a01fe3957f4981985532cd244d0665c4f2
piyush bhidikar <piyushbhidikar@gmail.com>
2025-07-31 23:11:53 +0530

    Update app.js with small change

--- app.js
+++ app.js
@@ -112,6 +112,9 @@ app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
 
+app.get('/', (req, res) => {
+    res.redirect('/listings');
+});
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
